f78409224909fb9dccd15cd42dad56f42c24ad24
Nam Hyeon-Woo <101378252+NHW93@users.noreply.github.com>
2026-03-23 17:47:13 +0900

    Revert "work on refactoring"
    This reverts commit c075fb8ba870e25faf4393379e54ef459f2bf0b8.

--- Assets/Scripts/Adapter/Manager/SkillManager.cs
+++ Assets/Scripts/Adapter/Manager/SkillManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using SwDreams.Data;
 using SwDreams.Adapter.Manager;
-using SwDreams.Adapter.Skill.TriggerEffects;
 
 namespace SwDreams.Adapter.Skill
 {
@@ -655,16 +654,6 @@ namespace SwDreams.Adapter.Skill
             }
 
             skill.Activate(skillData, effect);
-
-            // [Step 3-4] SkillTriggerSystem 초기화 (triggerEffects가 있는 경우)
-            if (skillData.triggerEffects != null && skillData.triggerEffects.Count > 0)
-            {
-                var triggerSystem = slotObj.GetComponent<SkillTriggerSystem>();
-                if (triggerSystem == null)
-                    triggerSystem = slotObj.AddComponent<SkillTriggerSystem>();
-                triggerSystem.Initialize(skillData.triggerEffects);
-            }
-
             return skill;
         }
 
